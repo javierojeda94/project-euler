ee06ae0f9a5148bbd7b082379e39847e0b2aec4a
Javier Ojeda <ojeda.javier94@gmail.com>
2018-07-31 17:33:52 -0500

    add problem 1 solution

---
+++ problem-1/problem_1.rb
@@ -0,0 +1,14 @@
+# https://projecteuler.net/problem=1
+class Problem1
+  def solve(num = 1000)
+    sum = 0
+    (1...num).each do |n|
+      sum += n if (n % 3).zero? || (n % 5).zero?
+    end
+    puts sum
+    sum
+  end
+end
+
+problem = Problem1.new
+problem.solve
