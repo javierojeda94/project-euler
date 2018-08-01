08b99742efdec4fe75c6416077e7db359d557c37
Javier Ojeda <ojeda.javier94@gmail.com>
2018-08-01 03:22:01 -0500

    add problem 3 solution

---
+++ problem-3/problem_3.rb
@@ -0,0 +1,18 @@
+# https://projecteuler.net/problem=3
+class Problem3
+  def solve(n = 600_851_475_143)
+    biggest_factor = 2
+    (2..(n / 2)).each do |i|
+      if (n % i).zero?
+        n /= i
+        biggest_factor = i
+      end
+      break if n == 1
+    end
+    puts biggest_factor
+    biggest_factor
+  end
+end
+
+problem = Problem3.new
+problem.solve
